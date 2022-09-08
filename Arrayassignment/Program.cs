9e852d7b1db9dcba97bded6d332418771e68239a
pc <pc@DESKTOP-P40ILKJ>
2022-09-08 12:58:29 +0100

    Add project files.

---
+++ Arrayassignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Arrayassignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNumbers = { 7, 2, 6, 15, 54, 10, 23 };
+            Console.WriteLine(myNumbers.Max());  // largest value
+            Console.WriteLine(myNumbers.Min());  // smallest value
+            Console.WriteLine(myNumbers.Sum());  // sum of myNumbers
+            Console.ReadLine();
+        }
+
+        public static int total(int Maxvalue, int Minvalue)
+        {   int[] numbers= {54, 2};
+            int total = 0
+            for (int i = 0; i < numbers.Length; i++)
+           
+            return total;
+        }
+
+    }
+
+}
+
